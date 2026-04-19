9e7774c0e5825d8d96dd029870f7c82dcb93358a
Ketan Behere <Ketanbehere555@gmail.com>
2026-04-19 10:30:49 +0530

    feat : added logging for user registration flow

--- src/main/java/com/ecommerce/user_service/service/UserService.java
+++ src/main/java/com/ecommerce/user_service/service/UserService.java
@@ -5,6 +5,8 @@ import com.ecommerce.user_service.dto.UserResponseDTO;
 import com.ecommerce.user_service.entity.User;
 import com.ecommerce.user_service.exception.UserAlreadyExistsException;
 import com.ecommerce.user_service.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,14 +15,18 @@ import java.time.LocalDateTime;
 @Service
 public class UserService {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private UserRepository userRepository;
 
     public UserResponseDTO registerUser(UserRequestDTO request) {
 
+     logger.info("Registering user with email:{}",request.getEmail());
         // 1. Check duplicate
         User existingUser = userRepository.findByEmail(request.getEmail());
         if (existingUser != null) {
+            logger.error("User already exists with email: {}", request.getEmail());
             throw new UserAlreadyExistsException("Email already exists");
         }
 
@@ -35,9 +41,11 @@ public class UserService {
 
         user.setCreatedAt(LocalDateTime.now());
         user.setCreatedBy("SYSTEM");
+        logger.info("saving user to database");
 
         // 3. Save
         User savedUser = userRepository.save(user);
+        logger.info("User saved with id:{}",savedUser.getId());
 
         // 4. Map Entity → Response DTO
         UserResponseDTO response = new UserResponseDTO();
